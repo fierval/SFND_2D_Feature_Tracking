c8f633d8384b3bb3ad6d087ff4d9b911dce7d54d
Boris Kogan <fierval@gmail.com>
2021-08-28 15:49:13 -0700

    Project task 1

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -25,7 +25,7 @@ int main(int argc, const char *argv[])
     /* INIT VARIABLES AND DATA STRUCTURES */
 
     // data location
-    string dataPath = "../";
+    string dataPath = "../../../";
 
     // camera
     string imgBasePath = dataPath + "images/";
@@ -38,6 +38,7 @@ int main(int argc, const char *argv[])
     // misc
     int dataBufferSize = 2;       // no. of images which are held in memory (ring buffer) at the same time
     vector<DataFrame> dataBuffer; // list of data frames which are held in memory at the same time
+
     bool bVis = false;            // visualize results
 
     /* MAIN LOOP OVER ALL IMAGES */
@@ -62,7 +63,16 @@ int main(int argc, const char *argv[])
         // push image into data frame buffer
         DataFrame frame;
         frame.cameraImg = imgGray;
-        dataBuffer.push_back(frame);
+        if (imgIndex >= dataBufferSize) {
+          // rotate out
+          std::rotate(dataBuffer.begin(), dataBuffer.begin() + 1, dataBuffer.end());
+
+          // insert at the end
+          *dataBuffer.rbegin() = frame;
+        }
+        else {
+          dataBuffer.push_back(frame);
+        }
 
         //// EOF STUDENT ASSIGNMENT
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
